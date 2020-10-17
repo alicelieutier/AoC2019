77d798f563f84fe9e7e8de9bc805720bd9a0941a
Alice Lieutier <alice@lieutier.me>
2020-10-17 15:14:46 +0100

    5.py

---
+++ students.rb
@@ -0,0 +1,107 @@
+# October
+Alex Clifford 
+Alistair Phipps
+Amy Balmforth
+Andy Jones
+Arjun Roy
+Ben Stuart
+Christopher Barnett
+Donald Newholm
+Ed Courage
+Elfi Yang 
+Ellie Aston
+Emma Van Oss
+Faduma Hussein
+Freddie Smith Hughes
+Ingrid Bjarman
+Kenny Kehinde Peter Olofinmoyin
+Liat Moss
+Mathew Freeman
+Muna Khan
+Natalie Yeo
+Robert Rosiji
+Robert Woolley 
+Sara Rancati
+Shubinder Virk
+Tenzin Ball
+
+## Nov
+
+Alastair Gilles
+Andrea Diotallevi
+Andrew Bacon
+Ben Sheridan-Edwards
+Charlie Paterson
+Daniel Martinez-Gatell
+David Bacall 
+Dawid Szpener
+Ellie Manifold
+Eve Noirault 
+Harrison Yan
+Harry Mumford
+Hisham Bokhari
+Jade Beresford
+Jamie Wong
+Jonathan Palma
+Julie Cheng
+Kealan Heena
+Raluca Ciucu
+Sam Folorunsho
+Shafique Mohammed
+Tuan Nguyen
+Zak Rashid 
+
+## Dec
+
+Alicia Merlino
+Asia Ellis
+Bassel Al-Sayed
+Ben Auld 
+Ben Gittins 
+Christopher Wood
+David Papamichael
+Emily Spencer
+Ilias Grigoropoulos
+Imraan Ali 
+Jack Willee
+Jake Phillips
+Katie Rodgers
+Lucian Boatright-Roberts
+Marcello Fabbri
+Meghan Iankov
+Nicolas Raffray
+Niki Manoledaki
+Ria Patel
+Shadi Khazaei 
+Shaun Yuh Low
+Tom Ratcliffe
+Tom Walker
+Umberto Valente
+Vish Mayer Kakkad
+
+## Jan
+
+Barri Faryad
+Christopher Eadie
+Dafna Libman
+David McGregor
+David Stewart
+Elena Beccaro
+Ellie Jones
+Emily Wright 
+Hansa Lallu
+Haydon Houghton
+Jack Overton
+Jason Wong 
+Joe Ellingworth
+Josh Blumberg
+Liz Daly
+Megan Hayes
+Myrtle Tzouma
+Paulo Azoia
+Peter Dean
+Richard Pattinson
+Sophie Worsnop
+Thomas-Oliver Greenaway
+Toby Dawson
+Ziad Soobratty
